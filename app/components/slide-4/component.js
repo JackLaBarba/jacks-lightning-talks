2aa69af51e3ee27465d16e20bbc52c2ff52094fb
dweebit <brakit@gmail.com>
2015-10-07 21:01:45 -0700

    Center map on our trip

--- app/components/slide-4/component.js
+++ app/components/slide-4/component.js
@@ -6,7 +6,9 @@ export default Ember.Component.extend({
   },
 
   initializeMap() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    let lat = 37.239908;
+    let lon = -118.680388;
+    let map = L.map('map').setView([lat, lon], 13);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
