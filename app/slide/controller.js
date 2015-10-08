7096150bcd933fd40949b2a7bb6e6194de207ebb
dweebit <brakit@gmail.com>
2015-10-07 19:56:28 -0700

    Add first slide

--- app/slide/controller.js
+++ app/slide/controller.js
@@ -4,6 +4,6 @@ export default Ember.Controller.extend({
   slideId: Ember.computed.alias('model.slideId'),
   slideComponentName: Ember.computed('slideId', function() {
     let slideId = this.get('slideId');
-    return `slide_${slideId}`;
+    return `slide-${slideId}`;
   })
 });
