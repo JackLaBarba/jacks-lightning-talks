04a55fd2d268343ff08fb28166d52aa600d2498c
dweebit <brakit@gmail.com>
2015-10-07 20:10:29 -0700

    Allow navigation between slides!

--- app/slide/controller.js
+++ app/slide/controller.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  slideId: Ember.computed.alias('model.slideId'),
+  slideId: Ember.computed('model.slideId', function() {
+    return parseInt(this.get('model.slideId'));
+  }),
   slideComponentName: Ember.computed('slideId', function() {
     let slideId = this.get('slideId');
     return `slide-${slideId}`;
+  }),
+
+  nextSlideId: Ember.computed('slideId', function () {
+    return this.get('slideId') + 1;
+  }),
+
+  lastSlideId: Ember.computed('slideId', function () {
+    return this.get('slideId') - 1;
   })
 });
--- app/slide/route.js
+++ app/slide/route.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return params;
+  },
+
+  actions: {
+    changeSlide(slideId) {
+      this.transitionTo('slide', slideId);
+    }
   }
 });
